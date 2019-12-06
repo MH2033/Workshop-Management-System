33725b6c98c8d323fec4642fba815ed5424ef720
MH2033 <mh.akbari@yahoo.com>
2019-12-06 09:21:17 +0330

    initial commit

---
+++ src/main/java/com/project/workshopmanagment/WorkshopmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.workshopmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/workshopmanagment/WorkshopmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workshopmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
